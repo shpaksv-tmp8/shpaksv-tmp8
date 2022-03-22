16995a47eb8c19e048d9f397fe4ee0e195f4a698
shpaksv-tmp8 <98120476+shpaksv-tmp8@users.noreply.github.com>
2022-03-22 17:41:21 -0400

    Create have-to-dance-with-cassie.py

---
+++ have-to-dance-with-cassie.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+'''
+Just another Saturday
+The boys are dressed up fine
+Think I'll wait 'till half past eight
+To dress myself divine
+Ten thirty, I'm in rags
+A slutty look accompanies my questionable gaze
+Through the strobe light and the haze
+
+Take an interest in me
+Why must I beg for a drink?
+Too verbose
+You come too close, Sir
+Please step back and think
+That's the trouble
+Flesh attracts the leery look
+Rarely does the dream boy come along
+He don't exist
+A lover with the candour of a friend
+
+I run away
+They're playing a decent song at last
+I think I'll have to dance with Cassie
+'Cause the dream boy never asked
+Shuffle to the left
+I kick the boy behind to make a little room
+Boogie to the right
+Cassie dances madly like a boxing kangaroo
+Her little Joey buys the drinks
+He's in love more than he thinks
+
+They know the dance too well
+The bodies heavenly
+Move around the autumn sky
+With measured dignity
+But my Venus and my Mars
+Are set to flash
+The lover will lay down his sword and swear
+He don't exist
+A brother to me when the chips were down
+
+I run away
+They're playing a decent song at last
+I think I'll have to dance with Cassie
+'Cause the dream boy never asked
+Shuffle to the left
+I kick the boy behind to make a little room
+Boogie to the right
+Cassie dances madly like a boxing kangaroo
+Spinnin' on the spot
+Hell do I care what I look like when I feel this good
+
+I'll keep on dancing
+I'll keep on dancing
+I'll keep on dancing
+I'll keep on dancing
+I'll keep on dancing
+'''
